1e5b1e8b17ea2b2855ca0fa6392a6086154f2826
IPbianco <ignaciopanebianco@gmail.com>
2017-10-03 20:20:27 +0000

    3_array.rb

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,13 @@
 
 class String
   def every_other_char
+    str = String.new
+  	arr = split('')
+  	length.times do |n|
+  		if n % 2 == 0
+  			str += arr[n]
+  		end
+  	end
+  	return str
   end
 end
