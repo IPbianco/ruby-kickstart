da72fd3abcdab62de4bc52a2ff85d80f54ffe543
IPbianco <ignaciopanebianco@gmail.com>
2017-10-05 16:07:29 +0000

    4_hashes.rb

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,15 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+  array = string.split(' ')
+  jash = Hash.new { |this_hash, key| this_hash[key] = "$" }
+  i = 0
+  array.each do |n|
+  	jash[n]
+  	jash[n] = i if jash[n] == "$"
+  	i += 1
+  end
+  return jash
+end
