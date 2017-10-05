23dee6441c85ca709b913f74c65a05355db25e88
IPbianco <ignaciopanebianco@gmail.com>
2017-10-05 15:52:26 +0000

    3_hashes.rb

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+  array = string.split(' ')
+  jash = Hash.new { |this_hash, key| this_hash[key] = 0 }
+  cont = 0
+  array.each do |n|
+  	jash[n.downcase] += 1
+  end
+  return jash
+end
